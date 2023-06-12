a337c47f6d32f3530a2fb47721c0a9f0ab6e4db4
XimenaSanchezUwU <126804482+XimenaSanchezUwU@users.noreply.github.com>
2023-06-12 11:46:08 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -14,13 +14,13 @@ function setup() {
 function draw() {
   background(220);
   image(img, 0, 0, windowWidth, windowHeight);
-  for (let i = 0; 9 > i; i++){
+  for (let i = 0; 15 > i; i++){
   noStroke();
   textSize(90);
   fill(255, 20, 20)
   text('Eres un adulto', 340, 58);
   textSize(105);
   fill(191*i, 20*i, 0*i);
-  text('No deberias tener miedo', 230, 450+i*35);
+  text('No deberias tener miedo', 100, 450+i*35);
   }
 }
